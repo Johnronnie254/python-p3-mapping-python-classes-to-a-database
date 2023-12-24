8a9f10a4b7fed39ca5fb61b4e744cf8930c4091b
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-12-24 12:19:33 +0300

    ORM

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,41 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+
+# Assuming CONN and CURSOR are correctly initialized
+Song.create_table()
+
+hello = Song("hello", "adele")
+hello.save()
+
+despacito = Song("Despacito", "Vida")
+despacito.save()
+
+songs = CURSOR.execute('SELECT * FROM songs')
+[row for row in songs]
